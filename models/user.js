0567e91e7739230a6310c8ee7ece79a2ce982445
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-12-27 15:21:52 -0500

    add comment to clarify passport-local-mongoose plugin usage

--- models/user.js
+++ models/user.js
@@ -17,6 +17,7 @@ const userSchema = new Schema({
     default: false,
   },
 });
+// Add passport-local-mongoose plugin to userSchema
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
